ac0439f2d1a232d6c40271a6a4548b6e806321d6
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2019-02-23 18:00:23 +0100

    upgrade tooling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { AppContainer } from 'react-hot-loader';
 import configureStore, { history } from './store/configureStore';
 import Root from './components/Root';
 
-require('./favicon.ico'); // Tell webpack to load favicon.ico
+//require('./favicon.ico'); // Tell webpack to load favicon.ico
 require('./styles/index.scss');
 const store = configureStore();
 
